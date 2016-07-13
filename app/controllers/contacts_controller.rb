f6596ff2b69fd33365bbbbfb3f5e883f9aad4d72
Peter Jang <peter.x.jang@gmail.com>
2016-07-12 20:37:21 -0500

    Add all restful actions and views

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,41 @@
 class ContactsController < ApplicationController
-  def show_contact
-    @contact = Contact.first
-    render 'single_contact.html.erb'
+  def index
+    @contacts = Contact.all
+    render 'index.html.erb'
   end
 
-  def show_contacts
-    @contacts = Contact.all
-    render 'all_contacts.html.erb'
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    @contact = Contact.new(first_name: params[:first_name], last_name: params[:last_name])
+    @contact.save
+    render 'create.html.erb'
+  end
+
+  def show 
+    @contact = Contact.find_by(id: params[:id])
+    render 'show.html.erb'
+  end
+
+  def edit
+    @contact = Contact.find_by(id: params[:id])
+    render 'edit.html.erb'
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    # @contact.first_name = params[:first_name]
+    # @contact.last_name = params[:last_name]
+    # @contact.save
+    @contact.update(first_name: params[:first_name], last_name: params[:last_name])
+    render 'update.html.erb'
+  end
+
+  def destroy
+    @contact = Contact.find_by(id: params[:id])
+    @contact.destroy
+    render 'destroy.html.erb'
   end
 end
