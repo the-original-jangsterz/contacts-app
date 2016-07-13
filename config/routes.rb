77eb80ff8890545906486b5ffffaaefe3987dfdd
Peter Jang <peter.x.jang@gmail.com>
2016-07-12 20:09:15 -0500

    Add restful routes (missing actions and views)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get '/contact_info' => 'contacts#show_contact'
-  get '/all_contact_info' => 'contacts#show_contacts'
+  get '/contacts' => 'contacts#index'
+  get '/contacts/new' => 'contacts#new'
+  post '/contacts' => 'contacts#create'
+  get '/contacts/:id' => 'contacts#show'
+  get '/contacts/:id/edit' => 'contacts#edit'
+  patch '/contacts/:id' => 'contacts#update'
+  delete '/contacts/:id' => 'contacts#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
