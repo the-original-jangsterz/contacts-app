f5a66e7655efd50fa3259a6f6f40a13a784df2d4
Peter Jang <peter.x.jang@gmail.com>
2016-07-19 21:09:03 -0500

    Add home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/' => 'contacts#index'
+
   get '/contacts' => 'contacts#index'
   get '/contacts/new' => 'contacts#new'
   post '/contacts' => 'contacts#create'
