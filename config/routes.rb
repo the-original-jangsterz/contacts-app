272cee49c6704d5ba1777c2eb8ddf404c2dc64e8
Peter Jang <peter.x.jang@gmail.com>
2016-07-19 21:08:13 -0500

    Implement authentication via cheat sheet

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   get '/contacts/:id/edit' => 'contacts#edit'
   patch '/contacts/:id' => 'contacts#update'
   delete '/contacts/:id' => 'contacts#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716200310) do
+ActiveRecord::Schema.define(version: 20160720020046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20160716200310) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "address"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
