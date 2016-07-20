fe0db1d902477355850236fc4836aa5ab5614d15
Peter Jang <peter.x.jang@gmail.com>
2016-07-19 21:31:06 -0500

    Fix issue of index page when not logged in

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,8 +13,13 @@ class ContactsController < ApplicationController
     # @contacts = Contact.where(user_id: current_user.id)
 
     # STRAIGHT UP RAILS ASSOCIATIONS
-    @contacts = current_user.contacts
-    render 'index.html.erb'
+    if current_user
+      @contacts = current_user.contacts
+      render 'index.html.erb'
+    else
+      flash[:warning] = "You must be logged in to see this page!"
+      redirect_to '/login'
+    end
   end
 
   def new
